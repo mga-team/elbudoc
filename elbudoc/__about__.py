fec19797c9a8a3334e0e7e17d1128ef7dae86995
Christophe CHAUVET <christophe.chauvet@gmail.com>
2017-06-12 20:51:10 +0200

    Initialize python module

---
+++ elbudoc/__about__.py
@@ -0,0 +1,21 @@
+# This file is licensed under the terms of the BSD License.
+# See the LICENSE file in the root of this repository
+# for complete details.
+from __future__ import absolute_import, division, print_function
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "elbudoc"
+__summary__ = "ELectronic BUsiness DOCuments"
+__uri__ = "https://github.com/mga-team/elbudoc"
+
+__version__ = "0.1.0"
+
+__author__ = "Christophe CHAUVET"
+__email__ = "christophe.chauvet@gmail.com"
+
+__license__ = "BSD"
+__copyright__ = "Copyright 2016-2017 %s" % __author__
